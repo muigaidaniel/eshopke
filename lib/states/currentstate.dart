e66ef428f6551a1ab65eef458972f4a31d86e87d
muigaidaniel <54684310+muigaidaniel@users.noreply.github.com>
2021-03-05 17:14:49 +0300

    Worked on login with Provider

---
+++ lib/states/currentstate.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class CurrentState extends ChangeNotifier{
+  String _uid;
+  String _email;
+
+  FirebaseAuth _auth= FirebaseAuth.instance;
+
+  Future<bool> signUpUser(String email, String password) async{
+    bool returnValue= false;
+    try{
+      UserCredential _userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      if(_userCredential.user != null){
+        returnValue= true;
+      }
+    }catch(e){
+      print(e);
+    }
+
+    return returnValue;
+
+  }
+
+  Future<bool> loginUser(String email, String password) async{
+    bool returnValue= false;
+    try{
+      UserCredential _userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      if(_userCredential.user != null){
+        returnValue= true;
+      }
+    }catch(e){
+      print(e);
+    }
+
+    return returnValue;
+  }
+
+}
